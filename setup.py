650c1d22614cb30b69172e6ce4674ea7749d7976
nabrate@weinberg <nicolo.abrate@polito.it>
2022-02-22 08:51:25 +0100

    DEV setup for installation added

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ File: setup.py
 
 Description: Class for handling nuclear reactor geometries.
 """
-from setuptools import setup, find_packages
+from setuptools import find_packages
+from distutils.core import setup
+
+requirements = "requirements.txt"
 
 setup(
    name='coreutils',
@@ -14,14 +17,19 @@ setup(
    author_email='nicolo.abrate@polito.it',
    url='https://nicolo_abrate@bitbucket.org/nicolo_abrate/coreutils.git',
    package_name = ['coreutils'],
-   packages=setuptools.find_packages(),
+   packages=find_packages(),
    license='LICENSE.md',
-   description='',
+   description='Handle core geometry and data for reactor physics applications',
    long_description=open('README.md').read(),
+   test_suite="tests",
+   setup_requires=['pytest-runner'],
+   tests_require=['pytest'],
+   include_package_data=True,
    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: MIT License",
         "Operating System :: OS independent",
     ],
+    install_requires=open(requirements).read().splitlines(),
     python_requires='>=3.6',
 )
