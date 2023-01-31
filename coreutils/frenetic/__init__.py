8b997d0ca7f97e91f6c95390d4b33cbced7dc62b
Nicolò Abrate <nicolo.abrate@polito.it>
2023-01-31 15:11:08 +0100

    BUG minor bug fixed

--- coreutils/frenetic/__init__.py
+++ coreutils/frenetic/__init__.py
@@ -1,5 +1,3 @@
-from .NEoutput import NEoutput
-from .THoutput import THoutput
 from .InpGen import inpgen, makecommoninput, auxNE, auxTH, mkdir, print_coreutils_info
 from .InpNE import writemacro, writeNEdata, writeConfig, makeNEinput, mysavetxt
 from .InpTH import writeCZdata, makeTHinput, writeTHdata
